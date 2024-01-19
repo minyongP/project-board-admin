a2d933f36252d7ee37afd79bfe78ada6b64425a4
minyongP <86906386+Minon98@users.noreply.github.com>
2024-01-19 16:50:37 +0900

    #2 - 부트 프로젝트 초기화
    #1 에서 분석했던 내용을 토대로
    스프링 부트 2.7.0 뼈대를 세움
    개발 준비 완료

---
+++ src/test/java/com/example/projectboardadmin/ProjectBoardAdminApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.projectboardadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ProjectBoardAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
