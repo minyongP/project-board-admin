e31002752bcc0458771c4c237a7d708eeaad4383
minyongP <86906386+Minon98@users.noreply.github.com>
2024-01-26 22:32:01 +0900

    #10 - 뷰 컨트롤러, 기본 뷰, 테스트 기초 작성
    어드민에서 필요로 한 뷰는 다음 5가지
    
    * 메인 루트 뷰
    * 게시글 관리 뷰
    * 댓글 관리 뷰
    * 회원 관리 뷰
    * 어드민 회원 뷰
    
    이에 각 뷰를 담당할 컨트롤러 클래스와
    기본적인 최소한의 핸들러 메소드, 뷰 템플릿,
    정상적인 뷰 응답 케이스를 간단히 정의하는 컨트롤러 테스트 작성
    테스트만 통과하는 정도의 구현이고
    실제 비즈니스 로직 구현을 담고 있지 않음

---
+++ src/main/java/com/example/projectboardadmin/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.example.projectboardadmin.controller;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String root() {
+        return "forward:/management/articles";
+    }
+}
