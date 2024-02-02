d3b936c7c36b88fed4057d06a8ceb0af686ffe30
minyongP <86906386+Minon98@users.noreply.github.com>
2024-02-02 22:52:43 +0900

    #29 - 비즈니즈 로직 테스트, 컨트롤러 테스트 작성
    게시글 목록, 단일 게시글, 게시글 삭제 기능을 만들 예정이므로
    이들의 테스트를 작성함.

---
+++ src/main/java/com/example/projectboardadmin/service/ArticleManagementService.java
@@ -0,0 +1,25 @@
+package com.example.projectboardadmin.service;
+
+import com.example.projectboardadmin.dto.ArticleDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ArticleManagementService {
+
+    public List<ArticleDto> getArticles() {
+        return List.of();
+    }
+
+    public ArticleDto getArticle(Long articleId) {
+        return null;
+    }
+
+    public void deleteArticle(Long articleId) {
+
+    }
+
+}
