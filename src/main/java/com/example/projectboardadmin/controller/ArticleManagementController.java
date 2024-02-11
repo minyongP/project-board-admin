679f9c785120599821087ab3b20e52ba19ef9b15
minyongP <86906386+Minon98@users.noreply.github.com>
2024-02-11 22:45:22 +0900

    #37 - 게시글 관리 페이지 구현: 컨트롤러, 서비스, 뷰

--- src/main/java/com/example/projectboardadmin/controller/ArticleManagementController.java
+++ src/main/java/com/example/projectboardadmin/controller/ArticleManagementController.java
@@ -1,22 +1,41 @@
 package com.example.projectboardadmin.controller;
 
+import com.example.projectboardadmin.dto.response.ArticleResponse;
+import com.example.projectboardadmin.service.ArticleManagementService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RequestMapping("/management/articles")
 @Controller
 public class ArticleManagementController {
 
+    private final ArticleManagementService articleManagementService;
+
     @GetMapping
-    public String articles(
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC)Pageable pageable,
-            Model model
-            ) {
+    public String articles(Model model) {
+        model.addAttribute("articles",
+                articleManagementService.getArticles().stream().map(ArticleResponse::withoutContent).toList()
+        );
+
         return "management/articles";
+    }
+
+    @ResponseBody
+    @GetMapping("/{articleId}")
+    public ArticleResponse article(@PathVariable Long articleId) {
+        return ArticleResponse.withContent(articleManagementService.getArticle(articleId));
+    }
+
+    @PostMapping("/{articleId}")
+    public String deleteArticle(@PathVariable Long articleId) {
+        articleManagementService.deleteArticle(articleId);
+
+        return "redirect:/management/articles";
     }
 }
---
+++ src/main/java/com/example/projectboardadmin/dto/response/ArticleResponse.java
@@ -0,0 +1,29 @@
+package com.example.projectboardadmin.dto.response;
+
+import com.example.projectboardadmin.dto.ArticleDto;
+import com.example.projectboardadmin.dto.UserAccountDto;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.time.LocalDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ArticleResponse (
+        Long id,
+        UserAccountDto userAccount,
+        String title,
+        String content,
+        LocalDateTime createAt
+) {
+    public static ArticleResponse of(Long id, UserAccountDto userAccount, String title, String content, LocalDateTime createAt) {
+        return new ArticleResponse(id, userAccount, title, content, createAt);
+    }
+
+    public static ArticleResponse withContent(ArticleDto dto) {
+        return ArticleResponse.of(dto.id(), dto.userAccount(), dto.title(), dto.content(), dto.createdAt());
+    }
+
+    public static ArticleResponse withoutContent(ArticleDto dto) {
+        return ArticleResponse.of(dto.id(), dto.userAccount(), dto.title(), null, dto.createdAt());
+    }
+
+}
