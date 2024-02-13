32c889f891085c3bfca2f4abe1183bc2f061609f
minyongP <86906386+Minon98@users.noreply.github.com>
2024-02-13 22:43:40 +0900

    #39 - 어드민 회원 기능, 컨트롤러, 뷰 구현

--- src/main/java/com/example/projectboardadmin/service/AdminAccountService.java
+++ src/main/java/com/example/projectboardadmin/service/AdminAccountService.java
@@ -1,35 +1,40 @@
 package com.example.projectboardadmin.service;
 
+import com.example.projectboardadmin.domain.AdminAccount;
 import com.example.projectboardadmin.domain.constant.RoleType;
 import com.example.projectboardadmin.dto.AdminAccountDto;
 import com.example.projectboardadmin.repository.AdminAccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class AdminAccountService {
 
     private final AdminAccountRepository adminAccountRepository;
 
+    @Transactional(readOnly = true)
     public Optional<AdminAccountDto> searchUser(String username) {
-        return Optional.empty();
+        return adminAccountRepository.findById(username).map(AdminAccountDto::from);
     }
 
     public AdminAccountDto saveUser(String username, String password, Set<RoleType> roleTypes, String email, String nickname, String memo) {
-        return null;
+        return AdminAccountDto.from(adminAccountRepository.save(AdminAccount.of(username, password, roleTypes, email, nickname, memo)));
     }
 
+    @Transactional(readOnly = true)
     public List<AdminAccountDto> users() {
-        return List.of();
+        return adminAccountRepository.findAll().stream().map(AdminAccountDto::from).toList();
     }
 
     public void deleteUser(String username) {
-
+        adminAccountRepository.deleteById(username);
     }
 
 }
